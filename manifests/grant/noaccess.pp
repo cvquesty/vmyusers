26a08dae9a642e7b15fa2101e99f84e085039a1b
Jerald Sheets <jsheetsjr@paypal.com>
2016-09-26 06:44:39 -0700

    documenting noaccess manifest.

--- manifests/grant/noaccess.pp
+++ manifests/grant/noaccess.pp
@@ -1,4 +1,19 @@
 # Defined Type to create No-Access User
+# To call this defined type, you simply need source it from a profile
+# or higher-scoped manifest as follows:
+#
+#  vmyusers::grant::noaccess { $name:
+#    dbauth   => 'dbuser',   <- Administrative user in MySQL
+#    dbauthpw => 'dbauthpw', <- Administrative user's password
+#    user     => 'username', <- User you wish to grant access to
+#    password => 'password', <- User's password you are creating
+#    database => 'dbname',   <- Database you are granting access to
+#    location => 'location', <- Host from which to give the new user access
+#  }
+#
+# This routine runs a simple MySQL command line to revoke all but basic
+# connect access.  This "location" parameter can be 'localhost' or '%',
+# whichever is needed for your particular purposes.
 #
 define vmyusers::grant::noaccess (
 
