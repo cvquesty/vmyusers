952c13913231de0a31a8748140583cb20d61ae75
Jerald Sheets <jsheetsjr@paypal.com>
2016-09-26 06:46:26 -0700

    Documenting readonly manifest.

--- manifests/grant/readonly.pp
+++ manifests/grant/readonly.pp
@@ -1,4 +1,19 @@
 # Defined Type to create Read-Only User
+# To call this defined type, you simply need source it from a profile
+# or higher-scoped manifest as follows:
+#
+#  vmyusers::grant::admin { $name:
+#    dbauth   => 'dbuser',   <- Administrative user in MySQL
+#    dbauthpw => 'dbauthpw', <- Administrative user's password
+#    user     => 'username', <- User you wish to grant access to
+#    password => 'password', <- User's password you are creating
+#    database => 'dbname',   <- Database you are granting access to
+#    location => 'location', <- Host from which to give the new user access
+#  }
+
+# This routine runs a simple MySQL command line to add a user and give it
+# Read-only access.  This "location" parameter can be 'localhost' or '%',
+# whichever is needed for your particular purposes.
 #
 define vmyusers::grant::readonly (
 
