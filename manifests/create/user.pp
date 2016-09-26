23687fc1df10381eaae3d63e8585c5ad34589455
Jerald Sheets <jsheetsjr@paypal.com>
2016-09-26 06:34:22 -0700

    Added documentation to the user addition routine.

--- manifests/create/user.pp
+++ manifests/create/user.pp
@@ -1,4 +1,18 @@
 # Defined Type to create a user under MySQL
+# To call this defined type, you simply need source it from a profile
+# or higher-scoped manifest as follows:
+#
+# vmyusers::create::user { 'username':
+#   dbauth   => 'dbuser',    <- Administrative user in MySQL
+#   dbauthpw => 'dbauthpw',  <- Administrative user's password
+#   user     => 'username',  <- User you wish to create
+#   password => 'passsword', <- User's password you are creating
+#   location => 'location',  <- host from which to give the new user access
+# }
+#
+# This routine runs a simple MySQL command line to add a user and give it
+# general access.  This "location" parameter can be 'localhost' or '%',
+# whichever is needed for your particular purposes.
 #
 define vmyusers::create::user (
 
@@ -6,7 +20,6 @@ define vmyusers::create::user (
   $dbauthpw,
   $user,
   $password,
-  $database,
   $location,
 
 ) {
