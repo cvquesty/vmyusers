591d0fa0b5899825b2254c07311e1764b9b0062c
Jerald Sheets <jsheetsjr@paypal.com>
2016-09-26 09:56:51 -0700

    adding flush before new rights to make sure old rights go away.

--- manifests/grant/readwrite.pp
+++ manifests/grant/readwrite.pp
@@ -28,7 +28,8 @@ define vmyusers::grant::readwrite (
 
   exec { 'create_readwrite_user':
     onlyif  => "/usr/bin/mysqladmin -u${user} -p\'${password}\' status",
-    command => "/usr/bin/mysql --user=${dbauth} --password=${dbauthpw} -e \"GRANT INSERT,SELECT,UPDATE ON ${database}.* TO \'${user}\'@\'${location}\' IDENTIFIED BY \'${password}\'; flush privileges;\"",
+    command => [  "/usr/bin/mysql --user=${dbauth} --password=${dbauthpw} -e \"REVOKE ALL PRIVILEGES,GRANT OPTION FROM \'${user}  \'@\'${location}\'; FLUSH PRIVILEGES;",
+                  "/usr/bin/mysql --user=${dbauth} --password=${dbauthpw} -e \"GRANT INSERT,SELECT,UPDATE ON ${database}.* TO \'${user}\'@\'${location}\' IDENTIFIED BY \'${password}\'; flush privileges;\"",],
   }
 
 }
