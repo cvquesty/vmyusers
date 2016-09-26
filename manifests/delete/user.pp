987535e51e1773961039d8feb61768058631f299
Jerald Sheets <jsheetsjr@paypal.com>
2016-09-26 12:01:37 -0700

    seeing if capitalization has anything to do with it.

--- manifests/delete/user.pp
+++ manifests/delete/user.pp
@@ -22,7 +22,7 @@ define vmyusers::delete::user (
 
   exec { 'delete_user':
     onlyif  => "/usr/bin/mysql --user=${dbauth} --password=${dbauthpw} -e \"use mysql; SELECT ${user} FROM user;’ | grep ${user} \"",
-    command => "/usr/bin/mysql --user=${dbauth} --password=${dbauthpw} -e \"DELETE FROM mysql.user WHERE User =\'${user}\';FLUSH PRIVILEGES;\"",
+    command => "/usr/bin/mysql --user=${dbauth} --password=${dbauthpw} -e \"DELETE FROM mysql.user WHERE User =\'${user}\';flush privileges;\"",
   }
 
 }
